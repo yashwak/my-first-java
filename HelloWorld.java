56b7ac48a7cbdb64774949bda66c13fb752921c9
james <james@example.com>
2019-08-26 02:57:32 +0000

    My third commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
      public static void main(String[] args) {
 	 //This is simple HelloWorld 
-	    System.out.println("Hello, World Ekna 26.08.2019 second commit");
+	    System.out.println("Hello, World Ekna 26.08.2019 Third commit");
 		
 	 }
 }
